2ffa6e8f985a4dfc2022d6586a5cb19b1034c4f9
曹大千 <caodaqian@bytedance.com>
2024-02-02 08:33:14 +0000

    feat:vim keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,9 @@ vim.keymap.set({ "n", "i" }, "<A-p>", "<cmd>pu<cr>", { desc = "Paste on next lin
 vim.keymap.set({ "n", "v" }, "<leader>z", "<C-W><C-\\|><C-W><C-_>", { desc = "Zoom one pane" })
 
 vim.keymap.set({ "i" }, "jj", "<ESC>", { desc = "Turn to Normar Mode" })
+vim.keymap.set({ "i" }, "<C-l>", "<Right>", { desc = "Move cursor right" })
+vim.keymap.set({ "i" }, "<C-h>", "<Left>", { desc = "Move cursor left" })
+vim.keymap.set({ "i" }, "<C-j>", "<Down>", { desc = "Move cursor down" })
+vim.keymap.set({ "i" }, "<C-k>", "<Up>", { desc = "Move cursor up" })
+vim.keymap.set({ "i" }, "<m-l>", "<C-o>%", { desc = "Move cursor next brackets" })
+vim.keymap.set({ "i" }, "<m-h>", "<C-o>%", { desc = "Move cursor next brackets" })
--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,12 +1,62 @@
 return {
 	{
-		"hrsh7th/nvim-cmp", -- The completion plugin
-		dependencies = {
-			"hrsh7th/cmp-cmdline", -- cmdline completions
-		},
+		"hrsh7th/nvim-cmp",
 		opts = function(_, opts)
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+
+			opts.mapping = vim.tbl_extend("force", opts.mapping, {
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+					-- this way you will only jump inside the snippet region
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			})
+		end,
+	},
+	{
+		"hrsh7th/cmp-cmdline", -- cmdline completions
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		config = function(_, opts)
+			local cmp = require("cmp")
+			-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline("/", {
+				sources = { { name = "buffer" } },
+			})
+			cmp.setup.cmdline("?", {
+				sources = { { name = "buffer" } },
+			})
+
 			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			require("cmp").setup.cmdline(":", {
+			cmp.setup.cmdline(":", {
 				sources = {
 					{ name = "cmdline" },
 					{ name = "path" },
