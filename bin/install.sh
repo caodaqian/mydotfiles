88ad7944201c5473cd751cda963fd018cd4448aa
cdq <cdq-c@qq.com>
2025-05-05 16:49:57 +0800

    feat:add bottom install laucher

--- bin/install.sh
+++ bin/install.sh
@@ -35,7 +35,7 @@ function main() {
 
 # install dependences software
 function brew_install() {
-	sofrware_list=(rg gnu-sed git curl gcc cmake nodejs lf tmux dust duf btop tldr eza lazygit gping dog fzf neovim pipx)
+	sofrware_list=(rg gnu-sed git curl gcc cmake nodejs tmux dust duf tldr eza lazygit gping dog fzf neovim pipx bottom)
 	brew install ${sofrware_list[*]}
 
 	### install neovim plugin
