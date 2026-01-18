4bf010b322dae3406633d6bccef6d3284e04e999
cdq <cdq-c@qq.com>
2026-01-18 23:07:13 +0800

    fix:rift

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,10 @@
 return {
 	{
 		"saghen/blink.cmp",
-		opts = function(_, opts)
-			opts.appearance.nerd_font_variant = "default"
-			opts.keymap.preset = "super-tab"
-		end,
+		opts = {
+			keymap = {
+				preset = "super-tab",
+			},
+		},
 	},
 }
